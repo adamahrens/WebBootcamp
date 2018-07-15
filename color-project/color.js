11edb83ce6526567fe5c9527cc95234d8c978ca2
Adam Ahrens <adam.aahrens@gmail.com>
2018-07-15 09:20:40 -0500

    Making progress on the game.

--- color-project/color.js
+++ color-project/color.js
@@ -1,18 +1,47 @@
-var colors = ["rgb(255, 0, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)", "rgb(180, 180, 180)", "rgb(10, 139, 70)", "rgb(111, 23, 79)"];
+var colors = randomColors(6);
+var pickedColor = pickRandomColor(colors);
+var message = document.querySelector("#message");
 var squares = document.getElementsByClassName('square');
-var pickedColor = colors[2];
+document.getElementById('color-to-guess').textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function(){
-    console.log('Clicked a square');
-
     if (this.style.backgroundColor == pickedColor) {
-      alert('You won!');
+      message.textContent = "Correct!";
+      setColors(squares, pickedColor);
+      setColor(document.getElementsByTagName("h1")[0], pickedColor);
     } else {
-      this.style.backgroundColor = "#232323";
+      setColor(this, "#232323");
+      message.textContent = "Try Again";
     }
   });
 }
 
-document.getElementById('color-to-guess').textContent = pickedColor;
+function setColors(arr, color) {
+  for (var i= 0; i < arr.length; i++) {
+    setColor(arr[i], color);
+  }
+}
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+function pickRandomColor(colors) {
+  var number = Math.floor(Math.random() * colors.length);
+  return colors[number];
+}
+
+function randomColors(number) {
+  var colors = [];
+  for(var i = 0; i < number; i++) {
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256)  ;
+    var rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
+    colors.push(rgb);
+  }
+
+  return colors;
+}
